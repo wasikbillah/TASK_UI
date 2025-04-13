eff2ab32951f439f73c4697b00c6d0257861bf88
Wasik <wasikbillah818@gmail.com>
2025-04-13 16:07:32 +0600

    new thing added

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TASK_UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TASK_UI.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string? Message { get; set; }
+    }
+}
